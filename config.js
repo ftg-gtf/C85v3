08d60e934cb9008601dc0d23a23096adbaf49a75
ftg-gtf <67550388+ftg-gtf@users.noreply.github.com>
2021-07-18 22:37:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig={
+    apiKey: "AIzaSyCaN3FwjhvLRG5SlIs7aeYS6wM1BK1h8vo",
+    authDomain: "storyteller-f8f38.firebaseapp.com",
+    projectId: "storyteller-f8f38",
+    storageBucket: "storyteller-f8f38.appspot.com",
+    messagingSenderId: "496542460509",
+    appId: "1:496542460509:web:ced957ad96ca8de097fbb4"
+}
